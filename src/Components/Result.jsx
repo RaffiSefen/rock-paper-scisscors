0b36c89033121d70b277016d7b0faef41030e0b2
Raf Sef <romanyforraffi@gmail.com>
2021-12-27 16:13:36 +0000

    feat: check score between user and computer

--- src/Components/Result.jsx
+++ src/Components/Result.jsx
@@ -1,5 +1,15 @@
-const Result = () => {
-  return <div></div>
+const Result = (props) => {
+  const { userScore, computerScore, success } = props
+  return (
+    <div className={`text-center ${success ? "bg-success" : "bg-danger"}`}>
+      <h3 className="col-md-6" id="user-score">
+        You:{userScore}
+      </h3>
+      <h3 className="col-md-6" id="computer-score">
+        Computer:{computerScore}
+      </h3>
+    </div>
+  )
 }
 
 export default Result
